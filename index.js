cad1f0d01ba58a2951950b1d4d660832ef0f5fc3
cyntiaaishimwe <ishimwecynthiaa@gmail.com>
2023-05-24 16:29:18 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+document.body.style.backgroundColor= 'silver'
+// document.getElementsByTagName('body').style.backgound = 'blue'
+
+
+// Change the font color for h1 title tag to
+// green
+
+document.getElementById('title').style.color = 'Green'
+
+// Change the font case for h3 title tags to
+// uppercase
+
+// document.getElementsByName('h3').style.
+
+// Add one more fruit to the fruits list
+
+let li = document.createElement('li');
+li.innerHTML = 'Strawberries';
+document.getElementById('fruList').appendChild(li)
+ 
+// Add one more vegetable to the vegetables
+// list
+
+// let li = document.createElement('li');
+// li.innerHTML = 'egggplants';
+// document.getElementById('vegList').appendChild(li)
+
